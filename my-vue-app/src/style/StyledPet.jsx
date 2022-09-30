443d719cde444a5ce9a6a6a2dff32ec0bc1d7535
Pedro Victor Saraiva <pedrosaraivadev@gmail.com>
2022-09-30 15:24:29 -0300

    :lipstick: Style for PetCadastro

---
+++ my-vue-app/src/style/StyledPet.jsx
@@ -0,0 +1,70 @@
+import styled from "styled-components";
+
+export const DivPai = styled.div`
+    background-color: #4d5b93;
+    margin: 0;
+`
+
+export const StyledH1 = styled.h1`
+    font-size: 2rem;
+    color: #000;
+    text-align: center;
+    font-family: Arial, Helvetica, sans-serif;
+    padding-top: 2vh;
+`
+
+export const FormS = styled.form`
+    background-color: #ffb;
+    border: 2px solid #333;
+    box-shadow: 5px 5px 5px #333;
+    font-family: Arial, Helvetica, sans-serif;
+    padding: 20px;
+    text-align: center;
+    width: 30vw;
+    height: 47vh;
+    margin: 10px;
+    h2{ padding-bottom: 10px; }
+    div{
+        margin: 7px;
+    }
+`
+
+export const StyledDiv = styled.div`
+    background-color: #d0fd78;
+    border: 2px solid #333;
+    box-shadow: 5px 5px 5px #333;
+    font-family: Arial, Helvetica, sans-serif;
+    padding: 20px;
+    width: 500px;
+    height: 600px;
+    margin: 10px;
+    padding: 10px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    img{ 
+        width: 300px; 
+        height: 300px; 
+    }
+    p{  
+        margin-bottom: 5px; 
+        margin-top: 5px;
+        margin-right: 2rem;
+        max-width: 390px;
+        overflow-x: auto;
+    }
+`
+
+export const DivFicha = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    min-width: 100vw;
+`
+
+export const DivCadastro = styled.div`
+    margin-bottom: 4vh;
+    display: flex;
+    justify-content: center;
+`
